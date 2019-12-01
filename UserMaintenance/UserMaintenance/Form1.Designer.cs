75d69342e2b97d6c4e0695bb3279ff1db1790b3a
Gergő Holper <gergo.holper@stud.uni-corvinus.hu>
2019-12-01 10:56:28 +0100

    commit 3

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -28,7 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listUsers = new System.Windows.Forms.ListBox();
 			this.txtBoxLastName = new System.Windows.Forms.TextBox();
 			this.lblFirstName = new System.Windows.Forms.Label();
 			this.lblLastName = new System.Windows.Forms.Label();
@@ -36,14 +36,14 @@
 			this.txtBoxFirstName = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
-			// listBox1
+			// listUsers
 			// 
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.ItemHeight = 20;
-			this.listBox1.Location = new System.Drawing.Point(12, 32);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(332, 404);
-			this.listBox1.TabIndex = 0;
+			this.listUsers.FormattingEnabled = true;
+			this.listUsers.ItemHeight = 20;
+			this.listUsers.Location = new System.Drawing.Point(12, 32);
+			this.listUsers.Name = "listUsers";
+			this.listUsers.Size = new System.Drawing.Size(332, 404);
+			this.listUsers.TabIndex = 0;
 			// 
 			// txtBoxLastName
 			// 
@@ -78,6 +78,7 @@
 			this.btnAdd.TabIndex = 5;
 			this.btnAdd.Text = "button1";
 			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
 			// 
 			// txtBoxFirstName
 			// 
@@ -96,7 +97,7 @@
 			this.Controls.Add(this.lblLastName);
 			this.Controls.Add(this.lblFirstName);
 			this.Controls.Add(this.txtBoxLastName);
-			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.listUsers);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -106,7 +107,7 @@
 
 		#endregion
 
-		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listUsers;
 		private System.Windows.Forms.TextBox txtBoxLastName;
 		private System.Windows.Forms.Label lblFirstName;
 		private System.Windows.Forms.Label lblLastName;
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,12 +12,27 @@ namespace UserMaintenance
 {
 	public partial class Form1 : Form
 	{
+		BindingList<Entities.User> users = new BindingList<Entities.User>();
 		public Form1()
 		{
 			InitializeComponent();
 			lblLastName.Text = Resource1.LastName;
 			lblFirstName.Text = Resource1.FirstName;
 			btnAdd.Text = Resource1.Add;
+
+			listUsers.DataSource = users;
+			listUsers.ValueMember = "ID";
+			listUsers.DisplayMember = "FullName";
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			var u = new Entities.User()
+			{
+				LastName = txtBoxLastName.Text,
+				FirstName = txtBoxFirstName.Text
+			};
+			users.Add(u);
 		}
 	}
 }
